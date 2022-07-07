6f22494320c790502edab64e2bd63447aa9aec6c
nivedha02 <76787422+nivedha02@users.noreply.github.com>
2022-07-07 14:06:24 +0530

    basics.java file is added

---
+++ basics.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class basics {
+    public static void main(String[] args) {
+        // System.out.println("hogwards");
+
+        Scanner input=new Scanner(System.in);
+       // System.out.println(input.nextInt()); // to get integer as input
+
+       //System.out.println(input.next()); //for string till the space (i.e)apple is well => apple
+
+       System.out.println(input.nextLine()); //for string over the entire line (i.e)apple is sweet =>apple is sweet
+
+    }
+    
+}
